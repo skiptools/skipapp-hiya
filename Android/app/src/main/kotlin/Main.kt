f347a66053820de1fd70441d2976ddcfdf25e2ea
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-06-10 15:45:54 -0400

    Update project to match template

--- Android/app/src/main/kotlin/Main.kt
+++ Android/app/src/main/kotlin/Main.kt
@@ -22,6 +22,9 @@ import androidx.core.app.ActivityCompat
 
 internal val logger: SkipLogger = SkipLogger(subsystem = "hiya.skip", category = "HiyaSkip")
 
+private typealias AppRootView = HiyaSkipRootView
+private typealias AppDelegate = HiyaSkipAppDelegate
+
 /// AndroidAppMain is the `android.app.Application` entry point, and must match `application android:name` in the AndroidMainfest.xml file.
 open class AndroidAppMain: Application {
     constructor() {
@@ -31,6 +34,7 @@ open class AndroidAppMain: Application {
         super.onCreate()
         logger.info("starting app")
         ProcessInfo.launch(applicationContext)
+        AppDelegate.shared.onInit()
     }
 
     companion object {
@@ -56,6 +60,10 @@ open class MainActivity: AppCompatActivity {
             }
         }
 
+        // when using Messaging, uncomment to register for message receipt
+        //Messaging.messaging().onActivityCreated(this)
+        AppDelegate.shared.onLaunch()
+
         // Example of requesting permissions on startup.
         // These must match the permissions in the AndroidManifest.xml file.
         //let permissions = listOf(
@@ -69,33 +77,33 @@ open class MainActivity: AppCompatActivity {
     }
 
     override fun onStart() {
+        logger.info("onStart")
         super.onStart()
-        HiyaSkipAppDelegate.shared.onStart()
     }
 
     override fun onResume() {
         super.onResume()
-        HiyaSkipAppDelegate.shared.onResume()
+        AppDelegate.shared.onResume()
     }
 
     override fun onPause() {
         super.onPause()
-        HiyaSkipAppDelegate.shared.onPause()
+        AppDelegate.shared.onPause()
     }
 
     override fun onStop() {
         super.onStop()
-        HiyaSkipAppDelegate.shared.onStop()
+        AppDelegate.shared.onStop()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        HiyaSkipAppDelegate.shared.onDestroy()
+        AppDelegate.shared.onDestroy()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        HiyaSkipAppDelegate.shared.onLowMemory()
+        AppDelegate.shared.onLowMemory()
     }
 
     override fun onRestart() {
@@ -126,7 +134,7 @@ internal fun PresentationRootView(context: ComposeContext) {
     PresentationRoot(defaultColorScheme = colorScheme, context = context) { ctx ->
         val contentContext = ctx.content()
         Box(modifier = ctx.modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            HiyaSkipRootView().Compose(context = contentContext)
+            AppRootView().Compose(context = contentContext)
         }
     }
 }
